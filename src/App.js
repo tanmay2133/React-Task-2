b3b6822e978fe5b96f2fedefef15d883f12f18d4
tanmay2133 <tanmaynaik2003@gmail.com>
2023-10-14 16:36:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.css'; // Create a CSS file for styling
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Calculator extends Component {
+  constructor() {
+    super();
+    this.state = {
+      display: '0',
+    };
+  }
+
+  handleButtonClick = (value) => {
+    if (value === '=') {
+      try {
+        this.setState({ display: eval(this.state.display) });
+      } catch (error) {
+        this.setState({ display: 'Error' });
+      }
+    } else if (value === 'C') {
+      this.setState({ display: '0' });
+    } else if (value === 'B') {
+      this.setState({
+        display: this.state.display.slice(0, -1),
+      });
+    } else {
+      this.setState({
+        display:
+          this.state.display === '0' ? value : this.state.display + value,
+      });
+    }
+  };
+
+  renderButton(value) {
+    return (
+      <button key={value} onClick={() => this.handleButtonClick(value)}>
+        {value}
+      </button>
+    );
+  }
+
+  render() {
+    const buttons = [
+      '7', '8', '9', '/',
+      '4', '5', '6', 'x',
+      '1', '2', '3', '-',
+      '0', '.', '=', '+',
+      'C', 'B'
+    ];
+
+    return (
+      <div className="calculator">
+        <div className="display">{this.state.display}</div>
+        <div className="buttons">
+          {buttons.map((button) => this.renderButton(button))}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Calculator;
